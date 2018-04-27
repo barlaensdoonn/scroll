ed749f4c07b1e68b2f6a1632426178b8fcea25fb
barlaensdoonn <brandon.aleson@gmail.com>
2018-04-27 16:55:54 -0700

    make cleaner interface for controlling generator object

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ if __name__ == '__main__':
     # initialize the stepper motor
     pins = [4, 17, 27, 22]
     stepper = Stepper(pins)
-    stppr = stepper.dynamic_step()
-    stppr.send(None)
 
-    # initialize the data set and convert it to a range of pauses
+    # initialize the data set and convert it to a range of pause values
     my_data = Data()
     pauses = my_data.translate(new_min=0.1, new_max=0.005)
 
-    # step through the range of pauses
+    # step through the list of pause values
     for i in range(len(pauses)):
         print('year = {}   feet = {}   pause = {}'.format(my_data.years[i], my_data.max_feets[i], pauses[i]))
-        stppr.send(pauses[i])
+        stepper.step(pauses[i])
--- stepper.py
+++ stepper.py
@@ -18,6 +18,15 @@ class Stepper:
     def __init__(self, pins):
         self.pins = pins
         self.step_pins = [OutputDevice(pin) for pin in self.pins]
+        self.stepper = self._init_stepper()
+
+    def _init_stepper(self):
+        '''get the stepper generator object ready to use'''
+
+        stppr = self._gen_step()
+        stppr.send(None)
+
+        return stppr
 
     def _sequencer(self):
         while True:
@@ -28,29 +37,34 @@ class Stepper:
         for i in range(len(self.step_pins)):
             self.step_pins[i].on() if sequence[i] else self.step_pins[i].off()
 
-    def step(self, pause=0.01):
-        for sequence in self.sequences:
-            self._step(sequence)
-            sleep(pause)
-
-    def dynamic_step(self):
+    def _gen_step(self):
         for sequence in self._sequencer():
             pause = yield
             self._step(sequence)
             sleep(pause)
 
+    def nongen_step(self, pause=0.01):
+        '''
+        the non-generator object way to step the stepper.
+
+        this needs to be called in a while True loop.
+        just here for reference, will probably be removed in the near future.
+        '''
+
+        for sequence in self.sequences:
+            self._step(sequence)
+            sleep(pause)
+
+    def step(self, pause=0.01):
+        self.stepper.send(pause)
+
+
 if __name__ == '__main__':
     pins = [4, 17, 27, 22]
     stepper = Stepper(pins)
-    pause = 0.01
 
     try:
-        print('initializing stepper...')
-        stppr = stepper.dynamic_step()
-        stppr.send(None)
-        print('stepping the stepper')
-
         while True:
-            stppr.send(pause)
+            stepper.step()
     except KeyboardInterrupt:
         print('...user exit received...')
