8aca229bddde01cd073c86ca8225d20d05e78f26
barlaensdoonn <brandon.aleson@gmail.com>
2018-04-29 22:15:25 -0700

    proof of conecpt control 2 motors

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-# control speed of a stepper motor with an arbitrary range of values
-# 4/27/18
+# control speed of a stepper motor by mapping pause time
+# between steps to an arbitrary range of values
+# 4/15/18
+# updated 4/28/18
 
 from data import Data
 from stepper import Stepper
 
 
-if __name__ == '__main__':
-    # initialize the stepper motor
-    pins = [4, 17, 27, 22]
-    stepper = Stepper(pins)
+def initialize_data():
+    return Data()
 
-    # initialize the data set and convert it to a range of pause values
-    my_data = Data()
+
+def initialize_steppers():
+    pins = {
+        'clockwise': [4, 17, 27, 22],
+        'counter-clockwise': [5, 6, 13, 19]
+    }
+
+    steppers = {
+        'let_out': Stepper(pins['clockwise'], direction='clockwise'),
+        'take_up': Stepper(pins['counter-clockwise'], direction='counter-clockwise')
+    }
+
+    return steppers
+
+
+if __name__ == '__main__':
+    steppers = initialize_steppers()
+    my_data = initialize_data()
+
+    # convert datapoints into a range of pause values for stepper motor
     pauses = my_data.translate(new_min=0.1, new_max=0.005)
 
     # step through the list of pause values
     for i in range(len(pauses)):
         print('year = {}   feet = {}   pause = {}'.format(my_data.years[i], my_data.max_feets[i], pauses[i]))
-        stepper.step(pauses[i])
+        steppers['let_out'].step(pauses[i])
+        steppers['take_up'].step(pauses[i])
