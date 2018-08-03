4d19c2a616474e53a1b8f2cf790720afdf19e0e4
Unknown <brandon.aleson@gmail.com>
2018-08-03 16:19:19 -0700

    refactor for consistency

--- alphaproto/stepperweblib.py
+++ alphaproto/stepperweblib.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python3
-# classes to communicate with Nanotech N5-1-3 motor controller
+#!/usr/bin/python
+# classes to communicate with Nanotech N5-1-3 motor controller via REST API
 # authored by Gary Stein at AlphaProto June 2018
-# refactored for python 3 by Brandon Aleson July 2018
-# updated 7/6/18
+# refactored by Brandon Aleson July 2018
+# updated 8/3/18
 
-import time
-import struct
 import urllib2
+import urllib
 import httplib
+import struct
+import time
+import sys
 
 
 class StepperComms:
-    # type matters, only listed in documentation or object directory
+    # type matters; only listed in documentation or object directory
 
     # Signed 16 (short)
     typeS16 = 0
@@ -32,28 +34,28 @@ class StepperComms:
 
     def make_value(self, value, type):
         svalue = ""
-        if type == self.typeS16:
+        if(type == self.typeS16):
             s = struct.pack(">h", value)
             c = struct.unpack("<BB", s)
             svalue = '"%02X%02X"' % (c)
-        elif type == self.typeU16:
+        elif(type == self.typeU16):
             s = struct.pack(">H", value)
             c = struct.unpack("<BB", s)
             svalue = '"%02X%02X"' % (c)
-        elif type == self.typeS32:
+        elif(type == self.typeS32):
             s = struct.pack(">i", value)
             c = struct.unpack("<BBBB", s)
             svalue = '"%02X%02X%02X%02X"' % (c)
         # unsigned only
-        elif type == self.typeU32:
+        elif(type == self.typeU32):
             s = struct.pack(">I", value)
             c = struct.unpack("<BBBB", s)
             svalue = '"%02X%02X%02X%02X"' % (c)
-        elif type == self.typeS08:
+        elif(type == self.typeS08):
             s = struct.pack(">b", value)
             c = struct.unpack("<B", s)
             svalue = '"%02X"' % (c)
-        elif type == self.typeU08:
+        elif(type == self.typeU08):
             s = struct.pack(">B", value)
             c = struct.unpack("<B", s)
             svalue = '"%02X"' % (c)
@@ -62,9 +64,11 @@ class StepperComms:
     def set_register(self, host, index, subindex, value, type):
         # Create URL scheme
         url = self.make_url(host, index, subindex)
-        # Motor controller needs 1.0 not 1.1
-        httplib.HTTPConnection._http_vsn = 10
-        httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
+
+        # Motor controller needs 1.0 not 1.0
+        # httplib.HTTPConnection._http_vsn = 10
+        # httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
+
         # value needs to be in Hex with quotes
         svalue = self.make_value(value, type)
         request = urllib2.Request(url)
@@ -77,14 +81,15 @@ class StepperComms:
         # print html
         response.close()
         # Recommends wait after every command
-        time.sleep(.1)
+        time.sleep(.01)
 
     def get_register(self, host, index, subindex):
         # Create URL scheme
         url = self.make_url(host, index, subindex)
+
         # Motor controller needs 1.0 not 1.0
-        httplib.HTTPConnection._http_vsn = 10
-        httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
+        # httplib.HTTPConnection._http_vsn = 10
+        # httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
 
         # on get, open url
         response = urllib2.urlopen(url)
@@ -102,68 +107,85 @@ class StepperComms:
 
 
 class StepperControl:
-    control_world = 0x6040
-    status_word = 0x6041
-    profile_velocity = 0x6081
-    target_position = 0x607A
-    inputs = 0x60FD
-    actual_position = 0x6064
-    operating_mode = 0x6060
-    input_voltage_range = 0x3240
+        ControlWord = 0x6040
+        StatusWord = 0x6041
+        ProfileVelocity = 0x6081
+        TargetPosition = 0x607A
+        Inputs = 0x60FD
+        ActualPosition = 0x6064
+        OperatingMode = 0x6060
+        InputVoltageRange = 0x3240
+        ClosedLoop = 0x3202
 
-    def __init__(self, host):
-        self.host = host
-        # Create comms
-        self.comms = StepperComms()
-        # Set Input 1 to 24 Volt Range
-        self.comms.set_register(self.host, self.input_voltage_range, 0x06, 1, self.comms.typeU32)
-        # Set As Position Mode
-        self.comms.set_register(self.host, self.operating_mode, 0x00, 1, self.comms.typeU08)
+        def __init__(self, host):
+            self.host = host
+            # Create comms
+            self.comms = StepperComms()
+            # Set Input 1 to 24 Volt Range
+            self.comms.set_register(self.host, self.InputVoltageRange, 0x06, 1, self.comms.typeU32)
+            # Set As Position Mode
+            self.comms.set_register(self.host, self.OperatingMode, 0x00, 1, self.comms.typeU08)
 
-    # Speed between 0 and 250 (positive only)
-    # Already ramps up and down, max speed
-    # Position negative and positive
-    def move_relative(self, Speed, Steps):
-        # Set speed as unsigned number
-        self.comms.set_register(self.host, self.profile_velocity, 0, Speed, self.comms.typeU32)
-        # Set relative position in ticks (25000 = 1 revolution of motor shaft)
-        self.comms.set_register(self.host, self.target_position, 0, Steps, self.comms.typeS32)
+        # Speed between 0 and 250 (positive only)
+        # Already ramps up and down, max speed
+        # Position negative and positive
+        def move_relative(self, Speed, Steps):
+            # Set speed as unsigned number
+            self.comms.set_register(self.host, self.ProfileVelocity, 0, Speed, self.comms.typeU32)
+            # Set relative position in ticks (25000 = 1 revolution of motor shaft)
+            self.comms.set_register(self.host, self.TargetPosition, 0, Steps, self.comms.typeS32)
 
-        # Bit 4 start
-        # Bit 5 immediate
-        # Bit 6 Absolute 0 vs Relative 1
-        # Bit 8 halt
-        # Bit 9 speed is not changed until target, no braking?
-        # must be moved to completely
+            # Bit 4 start
+            # Bit 5 immediate
+            # Bit 6 Absolute 0 vs Relative 1
+            # Bit 8 halt
+            # Bit 9 speed is not changed until target, no braking?
+            # must be moved to completely
 
-        # Quick Stop, Enable Voltage
-        self.comms.set_register(self.host, self.control_world, 0, 0x06, self.comms.typeU16)
-        # Switch On
-        self.comms.set_register(self.host, self.control_world, 0, 0x07, self.comms.typeU16)
-        # Enable Operation, Set Relative Motion
-        self.comms.set_register(self.host, self.control_world, 0, 0x4F, self.comms.typeU16)
-        # Start
-        self.comms.set_register(self.host, self.control_world, 0, 0x5F, self.comms.typeU16)
+            # Quick Stop, Enable Voltage
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x06, self.comms.typeU16)
+            # Switch On
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x07, self.comms.typeU16)
+            # Enable Operation, Set Relative Motion
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x4F, self.comms.typeU16)
+            # Start
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x5F, self.comms.typeU16)
 
-    # Halt and clear everything?
-    def motor_halt(self):
-        self.comms.set_register(self.host, self.control_world, 0, 0x1000, self.comms.typeU16)
+        # Halt and clear everything?
+        def motor_halt(self):
+            # Halt Free Spin
+            # self.comms.set_register(self.host, self.ControlWord, 0, 0x1000, self.comms.typeU16)
+            # Halt powered?
+            # self.comms.set_register(self.host, self.ControlWord, 0, 0x1007, self.comms.typeU16)
 
-    def check_reached(self):
-        # In status? 0x6041
-        # Bit 10 Target Reached
-        # Bit 12 Point Acknowledged
+            # Cancel move
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x0007, self.comms.typeU16)
+            # Hold
+            self.comms.set_register(self.host, self.ControlWord, 0, 0x000F, self.comms.typeU16)
 
-        reach = self.comms.get_register(self.host, self.status_word, 0)
-        if reach & 0x0400:
-            return 1
-        else:
-            return 0
+            # Move Zero?
+            # self.move_relative(0, 0)
 
-    def check_flag(self):
-        flag = self.comms.get_register(self.host, self.inputs, 0)
-        # print flag
-        if flag & 0x10000 == 0:
-            return 1
-        else:
-            return 0
+        def check_reached(self):
+            # In status? 0x6041
+            # Bit 10 Target Reached
+            # Bit 12 Point Acknowledged
+
+            reach = self.comms.get_register(self.host, self.StatusWord, 0)
+            if(reach & 0x0400):
+                return 1
+            else:
+                return 0
+
+        def check_flag(self):
+            flag = self.comms.get_register(self.host, self.Inputs, 0)
+            # print flag
+            return 1 if(flag & 0x10000 == 0) else 0
+
+        def get_status(self):
+            v = self.comms.get_register(self.host, self.StatusWord, 0)
+            print "%04X" % (v)
+
+        def get_closed(self):
+            v = self.comms.get_register(self.host, self.ClosedLoop, 0)
+            print "%04X" % (v)
--- alphaproto/ticktest.py
+++ alphaproto/ticktest.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python3
-# authored by Gary Stein at AlphaProto June 2018
-# refactored for python 3 by Brandon Aleson July 2018
-# updated 7/6/18
-
 import sys
 import time
 import stepperweblib
 
-#  Direction in case one has to spin backwards
-feed_direction = 1
+# Direction in case one has to spin backwards
+feed_direction = -1
 take_direction = 1
+
+# run for ever
+always_run = 1
 
 # Estimated size (inches)
 feed_diameter = 47
 take_diameter = 3
-
-# percentage over, in case (10%)
-take_overage = 1.10
+# percentage over
+take_overage = 1.25
 ticksperrev = 25000
 pi = 3.1415926535897932384626433832795
 
@@ -24,29 +21,31 @@ feed_circumference = pi * feed_diameter
 take_circumference = pi * take_diameter
 
 # Movement in inches linearly
-move_length = 12
-# How much to move per tick / tock
+MoveLength = 12
+# How much to move per tick/tock
 MoveSteps = 4
+# only move distance of tick/tock
+# will be recalculated as needed
+feed_ticks = MoveLength / feed_circumference * ticksperrev * feed_direction
+take_ticks = MoveLength / take_circumference * ticksperrev * take_direction * take_overage
 
-# only move distance of tick / tock will be recalculated as needed
-feed_ticks = move_length / feed_circumference * ticksperrev * feed_direction
-take_ticks = move_length / take_circumference * ticksperrev * take_direction
-print("feed ticks: {}   take ticks: {}".format(feed_ticks, take_ticks))
+print "Ticks: ", feed_ticks, take_ticks
 
-# Calc a velocity based on radius? in inches per minute
-LinearVelocity = 5
+# Calc a velocity based on radius?
+# in inches per minute
+LinearVelocity = .5
 # Make Velocity is 250 (about 5.6 on smaller side)
 max_velocity = 250
 feed_velocity = LinearVelocity / feed_circumference * ticksperrev / 60
-take_velocity = LinearVelocity / take_circumference * ticksperrev / 60 * take_overage
+take_velocity = LinearVelocity / take_circumference * ticksperrev / 60
 
 # in case
-if feed_velocity > max_velocity:
+if(feed_velocity > max_velocity):
     feed_velocity = max_velocity
-if take_velocity > max_velocity:
+if(take_velocity > max_velocity):
     take_velocity = max_velocity
 
-print("feed velocity: {}   take velocity: {}".format(feed_velocity, take_velocity))
+print "Velocity: ", feed_velocity, take_velocity
 
 # Create connects to motor controllers
 feed = stepperweblib.StepperControl("10.0.1.70")
@@ -59,42 +58,46 @@ take = stepperweblib.StepperControl("10.0.1.71")
 # 3 = Move in Slack (stop when done, back to move
 state = 0
 
-move_left = 0
+feed.motor_halt()
+take.motor_halt()
+
+
+MoveLeft = 0
 count = 0
 while True:
+
     # state machine
-    if state == 0:
+    if(state == 0):
         # doing nothing, waiting for button
-        feed.motor_halt()
-        take.motor_halt()
+        # feed.motor_halt()
+        # take.motor_halt()
 
         button = feed.check_flag()
         # button pressed, move to button state
         # Using momentary (logic reversed)
-        if(button == 0):
+        if(button == 0 or always_run):
             state = 1
             # Store the total movement
-            move_left = move_length
-    elif state == 1:
+            MoveLeft = MoveLength
+    elif(state == 1):
         # check if button released
         button = feed.check_flag()
         if(button == 1):
             state = 2
             CurrLength = 0
             # Will moves in ticks
-            if move_left > MoveSteps:
+            if(MoveLeft > MoveSteps):
                 CurrLength = MoveSteps
-                move_left = move_left - MoveSteps
+                MoveLeft -= MoveSteps
             else:
-                CurrLength = move_left
-                move_left = 0
+                CurrLength = MoveLeft
+                MoveLeft = 0
             # Calculate ticks base on tick of whats left of a tick
             feed_ticks = CurrLength / feed_circumference * ticksperrev * feed_direction
-            take_ticks = CurrLength / take_circumference * ticksperrev * take_direction
+            take_ticks = CurrLength / take_circumference * ticksperrev * take_direction * take_overage
             # Do Actual movement
             feed.move_relative(feed_velocity, feed_ticks)
-
-    elif state == 2:
+    elif(state == 2):
         # moving check if done
         done = feed.check_reached()
         # when done, stop motor
@@ -104,24 +107,26 @@ while True:
             state = 3
             # Move in either ticks or what is left (has some overage)
             take.move_relative(take_velocity, take_ticks)
-
-    elif state == 3:
+    elif(state == 3):
         # check to see if tensioned
 
         # When tension is made, stop
         tension = take.check_flag()
-        # Happens once per tick / tock
-        if tension == 1:
+        # backup, full motion
+        bdone = take.check_reached()
+
+        # Happens once per tick/tock
+        if(tension == 1 or bdone == 1):
             take.motor_halt()
             # if there is more ticks, do another
-            if move_left > 0:
+            if(MoveLeft > 0):
                 state = 1
             # if there are no more, go to idle
             else:
                 state = 0
 
-    count = count + 1
-    print("count: {}   state: {}   left: {}".format(count, state, move_left))
+    count += 1
+    print "Count: ", count, " State: ", state,  " Left: ", MoveLeft
 
     sys.stdout.flush()
     time.sleep(.1)
